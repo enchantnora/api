7eb77e6d86475080a204d9d72f8d3c306a0285c0
enchantnora <enchantnora@gmail.com>
2026-04-25 22:23:12 +0900

    2026/04/25 22:23:12.50

--- Undo.bat
+++ Undo.bat
@@ -1,6 +1,11 @@
 @echo off
 cd /d P:\API
-git reset --hard HEAD~1
-
-git push -f api main
-git push -f origin main
+set /p yn_check="Which do you like (y/n)"
+IF %yn_check:Y=Y%==Y (
+    git reset --hard HEAD~1
+    
+    git push -f api main
+    git push -f origin main
+) ELSE (
+    echo you selected " n "
+)
